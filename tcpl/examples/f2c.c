ba3f641e86af0844f5de311c01e61df2193ed7ac
SK <stcurvature@gmail.com>
2016-04-24 11:27:13 +0530

    Initial version for fahr to cels conversion

---
+++ tcpl/examples/f2c.c
@@ -0,0 +1,31 @@
+/*
+ * This program converts fahrenheit to celsius
+ *
+ */
+
+#include <stdio.h>
+
+/* print fahrenheit-celsius table for 
+ * farh = 0, 20,....,300 
+ */
+
+int main(void)
+{
+	int fahr;
+	int celsius;
+	int lower;
+	int upper;
+	int step;
+
+	lower = 0;		/* lower limit of temperature table */
+	upper = 300;	/* upper limit of temperature table */
+	step  = 20;		/* step size */
+
+	fahr = lower;
+
+	while (fahr <= upper) {
+		celsius = 5 * (fahr - 32)/9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
